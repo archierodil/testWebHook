db3c8807402e8bfbd1775f82e46261d75bfeac8c
archierodil <45745632+archierodil@users.noreply.github.com>
2019-01-11 17:44:16 +0800

    Update app.js

--- app.js
+++ app.js
@@ -154,7 +154,7 @@ console.log('field = ' + change.field);
 console.log('event id = ' + change.value.event_id);
 console.log('verb = ' + change.value.verb);	    
 if(change.field == 'events'){
-	   
+//the code below gets the event name	   
 https.get('https://graph.facebook.com/' + change.value.event_id + '?fields=name&access_token=DQVJ2WGg4NGlrLXFVR2pWdkp1MWhPYUxoNllaZAXVtSEJqZAFg1ZAURDd1hQNFNneVRTTjA4Ry1EbXI2VXA4OVQ5aUlXbGFYOU9HOXR1djlKUG5FR2pyRzlQc1VwNDU5S1J6Yjdzb1lSU0o1ZA25NOFJUVm1leGVMR0lQVWFJT0tFako3d0ZAHY1hQR2ZAmUkFOTkExbHZAGd210bjNsdW84NjZAVeXBmUW9wbmlxaUx0YVBSMXlua25YaW9RTW52bmVrMlU0eWRhZAGdnc3lieWVyQUVFMQZDZD', (resp) => {
   let datafiona = '';
   resp.on('data',(chunk) => {
@@ -163,11 +163,13 @@ https.get('https://graph.facebook.com/' + change.value.event_id + '?fields=name&
 	
  // The whole response has been received. Print out the result.
   resp.on('end', () => {
-	  console.log("this is the end");
+	  //console.log("this is the end");
          
-	  console.log('after the parse');
+	//  console.log('after the parse');
 	  console.log('fiona = ' + JSON.stringify(datafiona));
-	  console.log('my name =' + JSON.parse(datafiona).name);
+	  let eventname = JSON.parse(datafiona).name;
+	  //console.log('my name =' + JSON.parse(datafiona).name);
+	  	  	  
 /*	  	
    https.get('https://script.google.com/macros/s/AKfycbx5m7fyjxlQfjoJXGPTT649xugH5iWpfShSuubluVBnjUkArSM/exec?wpEvent=' + change.value + '&wpID=' + change.id + '&wpName=' + JSON.parse(datafiona).name + '&wpVerb=' + change.value.verb , (resp) => {
   let datashrek = '';
@@ -193,9 +195,33 @@ https.get('https://graph.facebook.com/' + change.value.event_id + '?fields=name&
 }).on("error", (err) => {
   console.log("Error: " + err.message);
 });
-	    
-	
 
+	
+//the code below gets the user's name		  
+	  
+https.get('https://graph.facebook.com/' + group_id + '?fields=name&access_token=DQVJ2WGg4NGlrLXFVR2pWdkp1MWhPYUxoNllaZAXVtSEJqZAFg1ZAURDd1hQNFNneVRTTjA4Ry1EbXI2VXA4OVQ5aUlXbGFYOU9HOXR1djlKUG5FR2pyRzlQc1VwNDU5S1J6Yjdzb1lSU0o1ZA25NOFJUVm1leGVMR0lQVWFJT0tFako3d0ZAHY1hQR2ZAmUkFOTkExbHZAGd210bjNsdW84NjZAVeXBmUW9wbmlxaUx0YVBSMXlua25YaW9RTW52bmVrMlU0eWRhZAGdnc3lieWVyQUVFMQZDZD', (resp) => {
+  let datafiona = '';
+  resp.on('data',(chunk) => {
+	  datafiona += chunk;  
+  });
+	
+ // The whole response has been received. Print out the result.
+  resp.on('end', () => {
+	  //console.log("this is the end");
+         
+	  //console.log('after the parse');
+	  //console.log('fiona = ' + JSON.stringify(datafiona));
+	  let attendeename = JSON.parse(datafiona).name;	
+;
+
+});
+
+}).on("error", (err) => {
+  console.log("Error: " + err.message);
+});	
+
+console.log('eventname = ' + eventname);
+console.log('attendeename = ' + attendeename);	
 }
 else{
 console.log('before returned value' );  
